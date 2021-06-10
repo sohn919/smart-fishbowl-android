2088c988e3adf35a37ead9054bafb67bf3b2eec2
hu1won <ehql713@gmail.com>
2021-06-10 15:43:01 +0900

    init/splash/background

---
+++ app/src/main/java/com/github/hu1won/fishbowl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.hu1won.fishbowl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expansion_panel_sample_main);
+    }
+}
